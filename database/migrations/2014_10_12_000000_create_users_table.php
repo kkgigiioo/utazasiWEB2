412ce5b10d25795689b7b4d412166a0448b4dada
kkgigiioo <47388662+kkgigiioo@users.noreply.github.com>
2019-03-10 19:53:37 +0100

    navBar half done

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,11 +16,14 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('userGroup'); //group of users (example: admin, generalUser)
+            $table->unsignedInteger('travelID');
+
         });
     }
 
---
+++ database/migrations/2019_03_10_155705_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('country'); //country of travel
+            $table->string('city'); //city of travel
+            $table->string('content'); //travel content
+            $table->text('description'); //travel description
+            $table->date('startDate'); //Departures date
+            $table->Integer('price'); //travel price
+            $table->Integer('maxNum'); //Maximum number of candidates
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
