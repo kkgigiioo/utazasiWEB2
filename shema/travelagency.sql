82ceb7402ee909c5a213779e4f52b9602009bb59
kkgigiioo <kassaik@gain.uni-sopron.hu>
2019-04-12 13:23:25 +0200

    v0.02

--- shema/travelagency.sql
+++ shema/travelagency.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3306
--- Létrehozás ideje: 2019. Már 31. 17:52
--- Kiszolgáló verziója: 5.7.19
--- PHP verzió: 7.3.3
+-- Létrehozás ideje: 2019. Ápr 12. 11:16
+-- Kiszolgáló verziója: 5.7.24
+-- PHP verzió: 7.2.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -21,6 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `travelagency`
 --
+CREATE DATABASE IF NOT EXISTS `travelagency` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `travelagency`;
 
 -- --------------------------------------------------------
 
@@ -34,16 +36,16 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- A tábla adatainak kiíratása `migrations`
 --
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_03_10_155705_create_travels_table', 1);
+(4, '2014_10_12_000000_create_users_table', 1),
+(5, '2014_10_12_100000_create_password_resets_table', 1),
+(6, '2019_03_10_155705_create_travels_table', 1);
 
 -- --------------------------------------------------------
 
@@ -66,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `password_resets` (
 
 DROP TABLE IF EXISTS `travels`;
 CREATE TABLE IF NOT EXISTS `travels` (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `country` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `content` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -77,15 +79,7 @@ CREATE TABLE IF NOT EXISTS `travels` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- A tábla adatainak kiíratása `travels`
---
-
-INSERT INTO `travels` (`id`, `country`, `city`, `content`, `description`, `startDate`, `price`, `maxNum`, `created_at`, `updated_at`) VALUES
-(1, 'Italy', 'Rome', '4 days Rome tour', 'This is a four days tour for Rome with accommodation, breakfast, hot dinner', '2019-06-12', 254, 24, '2019-03-31 10:53:33', '2019-03-31 10:53:33'),
-(2, 'Croatia', 'Makarska', '5 days tour for Makarska', 'Makarska tour for 5 days with dinner', '2019-08-02', 590, 8, '2019-03-31 11:35:26', '2019-03-31 11:35:26');
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -95,7 +89,7 @@ INSERT INTO `travels` (`id`, `country`, `city`, `content`, `description`, `start
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email_verified_at` timestamp NULL DEFAULT NULL,
@@ -103,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
-  `userGroup` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phoneNumber` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `travelID` int(10) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -112,9 +106,9 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- A tábla adatainak kiíratása `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `userGroup`, `travelID`) VALUES
-(1, 'Kiss Bence', 'kissbence@gmail.com', '2019-03-10 16:35:37', '912ec803b2ce49e4a541068d495ab570', NULL, '2019-03-10 16:35:37', '2019-03-10 16:35:37', 'gender', 0),
-(2, 'Admin', 'admin@info.hu', '2019-03-10 16:38:35', '21232f297a57a5a743894a0e4a801fc3', NULL, '2019-03-10 16:38:35', '2019-03-10 16:38:35', 'admin', 0);
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `phoneNumber`, `travelID`) VALUES
+(1, 'Kristof Kassai', 'kassai.gigi@gmail.com', NULL, '$2y$10$6MmavKBGlt0vk2e6n93/kuoTk2Z4g2cG8EB6.PFr5bCXySJJzH7LC', '3Pbl2FH9rQ4kEJZwplZB1Si99ZAfz6i94b5THXUQvlxN1tAw7xJgKU6GRadv', '2019-04-10 18:00:31', '2019-04-10 18:00:31', '06707737745', 0),
+(2, 'Egervári Anna', 'egervari_anna@hotmail.hu', NULL, '$2y$10$rhp.99JRZBukO6FgDLaQnOWYGHD6bMqFV0hiishf.1KmbzjSH5EHG', 'DZXcM9OeKx', '2019-04-10 18:00:46', '2019-04-10 18:00:46', NULL, 0);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
